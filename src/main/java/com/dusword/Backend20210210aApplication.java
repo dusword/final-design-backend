30d9fc6c52c2559fe1e76c525fe70c34cf57fee4
lixiangjie <773983534@qq.com>
2021-02-10 13:48:46 +0800

    创建backend

---
+++ src/main/java/com/dusword/Backend20210210aApplication.java
@@ -0,0 +1,13 @@
+package com.dusword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend20210210aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend20210210aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dusword/Backend20210210aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dusword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend20210210aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
